1ec4cbf7ccad0318a7570da2c709b0747ae38193
張継 <kevin.zhang@deeperse.com>
2023-03-09 10:49:23 +0800

    最大公约数计算

---
+++ sugar/math.go
@@ -0,0 +1,24 @@
+package sugar
+
+func MaxDivisor[T int | int8 | int16 | int32 | int64 | uint | uint16 | uint32 | uint64](min, max T) T {
+
+	var maxDivisor T
+	complement := max % min
+	if complement != 0 {
+		maxDivisor = MaxDivisor(complement, min)
+	} else {
+		maxDivisor = min
+	}
+	return maxDivisor
+}
+
+func MaxDivisor4Slice[T int | int8 | int16 | int32 | int64 | uint | uint16 | uint32 | uint64](arr []T) T {
+
+	maxDivisor := arr[0]
+	for i := 1; i < len(arr); i++ {
+
+		maxDivisor = MaxDivisor(maxDivisor, arr[i])
+	}
+
+	return maxDivisor
+}
---
+++ sugar/math_test.go
@@ -0,0 +1,12 @@
+package sugar
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMaxDivisor4Slice(t *testing.T) {
+	nums := []int64{6, 12, 18}
+	divisor := MaxDivisor4Slice(nums)
+	fmt.Println(divisor)
+}
